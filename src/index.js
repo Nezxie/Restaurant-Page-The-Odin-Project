1fc01d0608ea7e2fc96b783d7985bd3e94243643
Agnieszka Zachariasiewicz <agnieszka.kanarcia@gmail.com>
2023-12-12 03:35:09 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('working gut')
+//home module.js
+//menu module.js
+//contact module.js
+//logic that displays correct module based on menu event listeners
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        smokyblack: '#16130Fff',
+        umber: '#62524Cff',
+        bistre: '#2D2014ff',
+        sealbrown: '#6D371Fff',
+        brownsugar: '#AD6433ff',
+        almostwhite: '#d4cfc5'
+      },
+    },
+  },
+  plugins: [],
+}
